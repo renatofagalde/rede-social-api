13efb91a3aac72a92373fb37494ea8a865fdb473
renato@likwi.com.br <renato@likwi.com.br>
2023-07-09 15:04:13 -0300

    configurações das rotas no mux

--- api/src/router/rotas/Rotas.go
+++ api/src/router/rotas/Rotas.go
@@ -1,6 +1,9 @@
 package rotas
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 // Rota representa o modelo das rotas para as apis
 type Rota struct {
@@ -8,4 +11,12 @@ type Rota struct {
 	Metodo      string
 	Funcao      func(http.ResponseWriter, *http.Request)
 	Autenticado bool
+}
+
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+	return r
 }
