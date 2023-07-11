db0c6eff5d9bdba4d27592da351265e46d1d0eeb
renato@likwi.com.br <renato@likwi.com.br>
2023-07-11 12:01:33 -0300

    exibindo o ID inserido

--- api/src/controller/usuario-criar.go
+++ api/src/controller/usuario-criar.go
@@ -16,23 +16,26 @@ func Criar(writer http.ResponseWriter, request *http.Request) {
 	payload, erro := io.ReadAll(request.Body)
 	if erro != nil {
 		log.Fatal(erro)
-		return
 	}
 
 	var usuario model.Usuario
 	if erro = json.Unmarshal(payload, &usuario); erro != nil {
-		writer.Write([]byte("Erro ao converter usuário para struct"))
+		log.Fatal(erro)
 	}
 	fmt.Println(usuario)
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		writer.Write([]byte("Erro ao conectar com o BD"))
-		return
+		log.Fatal(erro)
 	}
 	defer db.Close()
 
 	repositorio := repositorio.NovoRepositorioUsuario(db)
-	repositorio.Criar(usuario)
+	ID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	writer.Write([]byte(fmt.Sprintf("ID inserido é: %d", ID)))
 
 }
