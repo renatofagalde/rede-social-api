d021bae71d3cc360be3a8050a8ed578d707eddaf
renato@likwi.com.br <renato@likwi.com.br>
2023-07-11 11:59:24 -0300

    corpo do repositório para salvar um usuário

--- api/src/controller/usuario-criar.go
+++ api/src/controller/usuario-criar.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"api/src/banco"
 	"api/src/model"
+	"api/src/repositorio"
+	_ "api/src/repositorio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -29,30 +31,8 @@ func Criar(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 	defer db.Close()
-	statement, erro := db.Prepare("insert into usuarios (nome,email) values (?,?);")
-	if erro != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		writer.Write([]byte(fmt.Sprintf("Erro ao preparar insert: %s", erro)))
-		return
-	}
-	defer statement.Close()
 
-	insercao, erro := statement.Exec(usuario.Nome, usuario.Email)
-	if erro != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		writer.Write([]byte("Erro ao executar insert"))
-		return
-	}
-
-	id, erro := insercao.LastInsertId()
-	if erro != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		writer.Write([]byte("Erro ao inserir usuario"))
-		return
-	}
-
-	//status code
-	writer.WriteHeader(http.StatusCreated)
-	writer.Write([]byte(fmt.Sprintf("Usuário inserido. ID = %d", id)))
+	repositorio := repositorio.NovoRepositorioUsuario(db)
+	repositorio.Criar(usuario)
 
 }
--- api/src/repositorio/usuairos.go
+++ api/src/repositorio/usuairos.go
@@ -17,5 +17,19 @@ func NovoRepositorioUsuario(db *sql.DB) *usuarios {
 
 // inserir um usuario no banco de dados
 func (u usuarios) Criar(usuario model.Usuario) (uint64, error) {
-	return 0, nil
+	statement, erro := u.db.Prepare("insert into usuarios (nome, nick, email, senha) value (?,?,?,?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+	ID, erro := insercao.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+	return uint64(ID), nil
 }
