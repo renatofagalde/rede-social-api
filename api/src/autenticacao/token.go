ec3f0a72100079c2a1e6118407f3875889b899d7
renato@likwi.com.br <renato@likwi.com.br>
2023-07-12 20:21:02 -0300

    gerando um token assinado

---
+++ api/src/autenticacao/token.go
@@ -0,0 +1,17 @@
+package autenticacao
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+// criar token
+func CriarToken(id uint64) (string, error) {
+
+	permissoes := jwt.MapClaims{} //reivindicações
+	permissoes["authorized"] = true
+	permissoes["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	permissoes["id"] = id
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes) //passando o método de aasinatura
+	return token.SignedString([]byte("Secret"))                    //secret
+}
